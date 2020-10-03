d82ed19509849a54e8000693531a2ed573891cbf
@ibraheemkabir9 <ibraheemkabir9@gmail.com>
2020-10-03 15:21:37 +0100

    initial_setup

---
+++ src/components/DesktopPage.js
@@ -0,0 +1,51 @@
+import React, {useContext} from 'react';
+import {WebThemeContext} from 'unifyre-react-helper';
+
+export function DesktopPage({children,NavBar,Footer}) {
+    const theme = useContext(WebThemeContext);
+    const styles = themedStyles(theme);
+    return (
+    <div style={{...styles.fullScreen, ...styles.smallGap}}>
+        <div style={styles.navbarContainer}>
+            {NavBar}
+        </div>
+        <div style={{...styles.fullScreen, ...styles.innerContainer}}>
+            {children}
+        </div>
+        <div style={styles.footer}>
+            {Footer}
+        </div>
+    </div>
+    );
+}
+
+const themedStyles = theme => ({
+    footer: {
+        position: 'relative',
+        backgroundColor: 'white',
+        bottom: '0',
+        width: '100%'
+    },
+    navbarContainer:{
+        backgroundColor: 'white',
+        color: 'white',
+        borderBottomWidth: '1px',
+        borderBottomStyle: 'solid',
+        zindex: 1
+    },
+    innerContainer: {
+        minWidth: '50%',
+        margin: '0px auto',
+        marginTop: '3%',
+        position: 'relative',
+        minHeight: window.innerHeight,
+    },
+    fullScreen: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: window.innerHeight,
+        backgroundColor: theme.backgroundColor,
+    },
+    smallGap: {
+    }
+});
